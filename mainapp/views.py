763ca3165f5393820508f81b1e419a6a84c1d271
Your Name <gemberobert1@gmail.com>
2022-08-03 09:31:48 +0300

    1st commit

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def home(request):
     return render(request, "index.html")
 
-
 def signup(request):
+    
     return render(request, "register.html")
+
 
 def signin(request):
     return render(request, "login.html")
